a0c60303f9fb605f482f0727f43ed470c4be26db
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-09-29 12:31:22 -0400

    updated html, js, and css for color changing based on time of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,24 @@
    $(document).ready(function () {
 
        // Displays the current date in the header
-       var today = newDate();
-       var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-       var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-       var dateTime = date + ' ' + time;
+       var today = moment();
+       $("#currentDay").text(today.format("LLLL"));
        $(".saveBtn").on("click", function () {
            var value = $(this).siblings(".description").val();
            var time = $(this).parent().attr("id");
            localStorage.setItem(time, value);
        })
 
-       $("#9 .description").val(localStorage.getItem("9"));
-       $("#10 .description").val(localStorage.getItem("10"));
-       $("#11 .description").val(localStorage.getItem("11"));
-       $("#12 .description").val(localStorage.getItem("12"));
-       $("#13 .description").val(localStorage.getItem("13"));
-       $("#14 .description").val(localStorage.getItem("14"));
-       $("#15 .description").val(localStorage.getItem("15"));
-       $("#16 .description").val(localStorage.getItem("16"));
-       $("#17 .description").val(localStorage.getItem("17"));
-
+       $("#hour_9.description").val(localStorage.getItem("hour_9"));
+       $("#hour_10.description").val(localStorage.getItem("hour_10"));
+       $("#hour_11.description").val(localStorage.getItem("hour_11"));
+       $("#hour_12.description").val(localStorage.getItem("hour_12"));
+       $("#hour_13.description").val(localStorage.getItem("hour_13"));
+       $("#hour_14.description").val(localStorage.getItem("hour_14"));
+       $("#hour_15 description").val(localStorage.getItem("hour_15"));
+       $("#hour_16 description").val(localStorage.getItem("hour_16"));
+       $("#hour_17.description").val(localStorage.getItem("hour_17"));
+       $("#hour_18.description").val(localStorage.getItem("hour_18"));
 
 
        function cssUpdate() {
