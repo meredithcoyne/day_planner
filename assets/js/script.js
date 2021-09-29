dfea9219f8b267ed74f4aa1106ea702b52c2db27
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-09-29 00:04:05 -0400

    updated js code to include current date and time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,47 @@
-$(document).ready(function () {
+   $(document).ready(function () {
 
-    // Displays the current date in the header
-    var today = moment();
-    $('#currentDay').text(today.format('LLLL'));
-    $('.saveBtn').on('click', function () {
+       // Displays the current date in the header
+       var today = newDate();
+       var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+       var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+       var dateTime = date + ' ' + time;
+       $(".saveBtn").on("click", function () {
+           var value = $(this).siblings(".description").val();
+           var time = $(this).parent().attr("id");
+           localStorage.setItem(time, value);
+       })
 
-        var value = $(this).siblings('.description').val();
-        var time = $(this).parent().attr('id');
-        localStorage.setItem(time, value);
-    })
-
-    $('hour_9.description').val(localStorage.getItem('9'));
-    $('hour_10.description').val(localStorage.getItem('9'));
-    $('hour_11.description').val(localStorage.getItem('9'));
-    $('hour_12.description').val(localStorage.getItem('9'));
-    $('hour_13.description').val(localStorage.getItem('9'));
-    $('hour_14.description').val(localStorage.getItem('9'));
-    $('hour_15.description').val(localStorage.getItem('9'));
-    $('hour_16.description').val(localStorage.getItem('9'));
-    $('hour_17.description').val(localStorage.getItem('9'));
-    $('hour_18.description').val(localStorage.getItem('9'));
+       $("#9 .description").val(localStorage.getItem("9"));
+       $("#10 .description").val(localStorage.getItem("10"));
+       $("#11 .description").val(localStorage.getItem("11"));
+       $("#12 .description").val(localStorage.getItem("12"));
+       $("#13 .description").val(localStorage.getItem("13"));
+       $("#14 .description").val(localStorage.getItem("14"));
+       $("#15 .description").val(localStorage.getItem("15"));
+       $("#16 .description").val(localStorage.getItem("16"));
+       $("#17 .description").val(localStorage.getItem("17"));
 
 
 
-    function agendaSaved() {
-        var currentHour = today.hours();
-        $(".time-block").each(function () {
-                var divTimeId = parseInt($(this).attr("id")); console.log(divTimeId);
-            if (divTimeId < currentHour) {
-                $(this).addClass('past');
-            } else if (divTimeId === currentHour) {
-                $(this).removeClass('past');
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            } else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-                $(this).addClass("future");
-            }
+       function cssUpdate() {
+           var currentHour = today.hours();
+           $(".time-block").each(function () {
+               var divTimeId = parseInt($(this).attr("id"));
+               console.log(divTimeId);
+               if (divTimeId < currentHour) {
+                   $(this).addClass("past");
+               } else if (divTimeId === currentHour) {
+                   $(this).removeClass("past");
+                   $(this).removeClass("future");
+                   $(this).addClass("present");
+               } else {
+                   $(this).removeClass("past");
+                   $(this).removeClass("present");
+                   $(this).addClass("future");
+               }
+           })
+       }
 
-        })
+       cssUpdate();
 
-}
-
-cssUpdate();
-
-})
+   })
